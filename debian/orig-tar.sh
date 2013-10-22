2628bed8ec868229a44b2fed23cb514aae3617f0
Stephen Nelson <stephen@eccostudio.com>
2013-10-22 21:51:43 +0100

    Packaging new upstream release spring framework 3.2.4.
    Modified watch file to mangle the RELEASE string from the version.
    New upstream release added to changelog
    Use gradle to build

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -1,11 +1,18 @@
 #!/bin/sh -e
 
 # called by uscan with '--upstream-version' <version> <file>
-DIR=libspring-java-$2
-TAR=libspring-java_$2.orig.tar.gz
+VERSION=$2
+DIR=libspring-java-$VERSION
+TAR=../libspring-java_$VERSION.RELEASE.orig.tar.xz
+TAG="v$VERSION.RELEASE"
 
-# Repack upstream source to tar.gz
-unzip $3
-mv spring* $DIR
-GZIP=--best tar czf $TAR -X debian/orig-tar.exclude $DIR
-rm -rf $DIR
+git clone https://github.com/spring-projects/spring-framework.git $DIR
+cd $DIR
+git checkout tags/$TAG
+cd ..
+rm -rf $DIR/.git
+
+XZ_OPT=--best tar -c -J -f $TAR -X debian/orig-tar.exclude $DIR
+rm -rf $DIR $3
+
+exit 0
